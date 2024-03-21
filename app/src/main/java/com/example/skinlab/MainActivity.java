fc54678ef9d38aa2193d536c235685da2dbb4d84
Mai Huế <huevpm21411@st.uel.edu.vn>
2024-03-21 12:55:48 +0700

    viewBinding

--- app/src/main/java/com/example/skinlab/MainActivity.java
+++ app/src/main/java/com/example/skinlab/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.skinlab.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
