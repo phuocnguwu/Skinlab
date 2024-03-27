4f3a1a28323236fa57a7bf915eddb414534a11b1
nhudh <nhudh21411@st.uel.edu.vn>
2024-03-27 20:24:24 +0700

    test commit

---
+++ app/src/main/java/com/example/skinlab/Forum_AddReview.java
@@ -0,0 +1,14 @@
+package com.example.skinlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forum_AddReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum_add_review);
+    }
+}
