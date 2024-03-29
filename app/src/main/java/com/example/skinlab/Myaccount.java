1fcc6faad59f085ce977bb664ea3bba46685c13c
nguyenvumaithanh <thanhnvm21411@st.uel.edu.vn>
2024-03-29 20:28:43 +0700

    Button Save

--- app/src/main/java/com/example/skinlab/Myaccount.java
+++ app/src/main/java/com/example/skinlab/Myaccount.java
@@ -2,13 +2,63 @@ package com.example.skinlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.skinlab.databinding.ActivityMyaccountBinding;
 
 public class Myaccount extends AppCompatActivity {
+
+    ActivityMyaccountBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_myaccount);
+        binding = ActivityMyaccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        addEvents();
+    }
+
+    private void addEvents() {
+        binding.btneditavatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.btnmoronghoso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Myaccount.this, Myaccount_Profile.class);
+                startActivity(intent);
+            }
+        });
+        binding.btnmorongdonhang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.btnmorongdiachi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Myaccount.this, Myaccount_Diachi.class);
+                startActivity(intent);
+
+            }
+        });
+        binding.btnmorongcaidat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.btndangxuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/skinlab/Myaccount_Profile.java
+++ app/src/main/java/com/example/skinlab/Myaccount_Profile.java
@@ -1,14 +1,65 @@
 package com.example.skinlab;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.skinlab.databinding.ActivityDialogSaveBinding;
+import com.example.skinlab.databinding.ActivityMyaccountBinding;
+import com.example.skinlab.databinding.ActivityMyaccountProfileBinding;
 
 public class Myaccount_Profile extends AppCompatActivity {
+    ActivityMyaccountProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_myaccount_profile);
+        binding = ActivityMyaccountProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        addEvents();
+    }
+
+    private void addEvents() {
+        binding.btneditavatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.btnsavepf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlerDialog();
+            }
+
+            private void showAlerDialog() {
+                ActivityDialogSaveBinding dialogsaveBinding = ActivityDialogSaveBinding.inflate(LayoutInflater.from(Myaccount_Profile.this));
+                AlertDialog.Builder builder = new AlertDialog.Builder(Myaccount_Profile.this)
+                        .setView(dialogsaveBinding.getRoot())
+                        .setCancelable(true);
+
+                // Create dialog
+                final AlertDialog dialog = builder.create();
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().setLayout(200, 200);
+                dialog.show();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        dialog.dismiss();
+                    }
+                }, 1000);
+            }
+
+
+
+
+        });
     }
 }
