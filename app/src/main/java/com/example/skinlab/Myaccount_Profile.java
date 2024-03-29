e6fc246a594341bc96d0d12f1406f7e053f39e74
nguyenvumaithanh <thanhnvm21411@st.uel.edu.vn>
2024-03-29 10:35:34 +0700

    Giao diện my account_profile

---
+++ app/src/main/java/com/example/skinlab/Myaccount_Profile.java
@@ -0,0 +1,14 @@
+package com.example.skinlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Myaccount_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myaccount_profile);
+    }
+}
