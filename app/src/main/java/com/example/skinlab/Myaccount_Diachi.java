4caaca9855726de1796674736b41b99d00710096
nguyenvumaithanh <thanhnvm21411@st.uel.edu.vn>
2024-03-29 11:05:55 +0700

    Giao diện my account_diachi chưa xong

---
+++ app/src/main/java/com/example/skinlab/Myaccount_Diachi.java
@@ -0,0 +1,14 @@
+package com.example.skinlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Myaccount_Diachi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myaccount_diachi);
+    }
+}
