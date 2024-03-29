488c472cafae819bec7d563daac929d6200732e2
nhudh <nhudh21411@st.uel.edu.vn>
2024-03-30 00:24:53 +0700

    forum_bosung

--- app/src/main/java/com/example/skinlab/Forum_AddReview.java
+++ app/src/main/java/com/example/skinlab/Forum_AddReview.java
@@ -2,13 +2,40 @@ package com.example.skinlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.example.skinlab.databinding.ActivityForumAddReviewBinding;
 
 public class Forum_AddReview extends AppCompatActivity {
+    ActivityForumAddReviewBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forum_add_review);
+        binding = ActivityForumAddReviewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        addEvents();
+
+    }
+
+    private void addEvents() {
+        binding.btnAddReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Forum_AddReview.this, Forum_AddReview_Main.class);
+                startActivity(intent);
+
+            }
+        });
+        binding.lvReview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(Forum_AddReview.this, Forum_Detailed.class);
+            }
+        });
     }
 }
