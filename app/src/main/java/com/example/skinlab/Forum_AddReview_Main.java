0b33b4646d40e767a4b75977e11e0eae47327df4
nhudh <nhudh21411@st.uel.edu.vn>
2024-03-30 15:25:35 +0700

    forum_dialog_send_review

--- app/src/main/java/com/example/skinlab/Forum_AddReview_Main.java
+++ app/src/main/java/com/example/skinlab/Forum_AddReview_Main.java
@@ -1,14 +1,54 @@
 package com.example.skinlab;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.skinlab.databinding.ActivityDialogSaveBinding;
+import com.example.skinlab.databinding.ActivityForumAddReviewMainBinding;
+import com.example.skinlab.databinding.ActivityForumDialogSendBinding;
 
 public class Forum_AddReview_Main extends AppCompatActivity {
+    ActivityForumAddReviewMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forum_add_review_main);
+        binding = ActivityForumAddReviewMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        addEvents();
+    }
+
+    private void addEvents() {
+        binding.btnSendReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlerDialog();
+            }
+            private void showAlerDialog() {
+                ActivityForumDialogSendBinding forumdialogsendBinding = ActivityForumDialogSendBinding.inflate(LayoutInflater.from(Forum_AddReview_Main.this));
+                AlertDialog.Builder builder = new AlertDialog.Builder(Forum_AddReview_Main.this)
+                        .setView(forumdialogsendBinding.getRoot())
+                        .setCancelable(true);
+
+                // Create dialog
+                final AlertDialog dialog = builder.create();
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().setLayout(200, 200);
+                dialog.show();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        dialog.dismiss();
+                    }
+                }, 1000);
+            }
+        });
     }
 }
