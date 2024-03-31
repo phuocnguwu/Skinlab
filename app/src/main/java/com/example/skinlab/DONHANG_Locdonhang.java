0078259298a5e412a3e652dbdae3149567953af4
phuocng216 <phuocxyzz@example.com>
2024-04-01 02:20:27 +0700

    Đơn hàng

--- app/src/main/java/com/example/skinlab/DONHANG_Locdonhang.java
+++ app/src/main/java/com/example/skinlab/DONHANG_Locdonhang.java
@@ -3,12 +3,54 @@ package com.example.skinlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.adapters.CategoryAdapter;
+import com.example.models.Category;
+import com.example.skinlab.databinding.ActivityAboutskinIntroTestBinding;
+import com.example.skinlab.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DONHANG_Locdonhang extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    Spinner spncategory;
+    CategoryAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_donhang_locdonhang);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        spncategory = findViewById(R.id.spn_category);
+        adapter = new CategoryAdapter(this, R.layout.donhang_category_selecteditem, getListCategory());
+        spncategory.setAdapter(adapter);
+        spncategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(DONHANG_Locdonhang.this, "Vui lòng chọn bộ lọc!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private List<Category> getListCategory() {
+        List<Category> list = new ArrayList<>();
+        list.add(new Category("Đang xử lý"));
+        list.add(new Category("Đang vận chuyển"));
+        list.add(new Category("Đang giao hàng"));
+        list.add(new Category("Giao hàng thành công"));
+        list.add(new Category("Đã hủy"));
+        return list;
     }
 }
