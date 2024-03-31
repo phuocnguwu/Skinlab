8a13e42060f9c0c21c69169bd9141b633dd25ba4
vophucthao2908 <phucthaov@gmail.com>
2024-03-31 20:50:01 +0700

    main skin & intro test

---
+++ app/src/main/java/com/example/skinlab/Aboutskin_intro_test.java
@@ -0,0 +1,33 @@
+package com.example.skinlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.skinlab.databinding.ActivityAboutskinIntroTestBinding;
+
+public class Aboutskin_intro_test extends AppCompatActivity {
+
+    ActivityAboutskinIntroTestBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAboutskinIntroTestBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        TextView textView = findViewById(R.id.txtLuuy);
+
+        String fullText = "Đảm bảo bạn thực hiện trả lời đầy đủ các câu hỏi một cách khách quan."
+                +"\n\nDựa trên kết quả của test, chọn sản phẩm chăm sóc da phù hợp với tình trạng da của bạn, bao gồm sữa rửa mặt, kem dưỡng, và kem chống nắng."
+                +"\n\nTrong môi trường trực tuyến và tùy theo cảm nhận hiện tại của bạn về làn da, việc đánh giá có thể không được nhất quán, vì vậy kết quả mang tính chất tham khảo."
+                +"\n\nCó thể thực hiện kiểm tra tình trạng da nhiều lần trong thời gian khác nhau và trong điều kiện da khác nhau để đảm bảo tính chính xác của kết quả."
+                +"\n\nNếu bạn có bất kỳ thắc mắc nào khác về tình trạng da của mình, hãy tham khảo ý kiến của một chuyên gia da liễu hoặc chuyên gia làm đẹp. Họ có thể cung cấp lời khuyên và điều trị phù hợp nhất cho da của bạn.";
+
+
+        textView.setText(fullText);
+    }
+
+
+}
