ae4936164e79c4960555da988e51d911c12f36b7
vophucthao2908 <phucthaov@gmail.com>
2024-04-01 23:10:55 +0700

    skin chu trinh chua co adapter model

---
+++ app/src/main/java/com/example/skinlab/Aboutskin_chutrinh.java
@@ -0,0 +1,14 @@
+package com.example.skinlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aboutskin_chutrinh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutskin_chutrinh);
+    }
+}
