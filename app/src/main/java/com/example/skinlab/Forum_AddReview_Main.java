9e21087f8e1797402909a0c02492518f0ec039f3
nhudh <nhudh21411@st.uel.edu.vn>
2024-04-12 21:45:58 +0700

    dialog dat hang

--- app/src/main/java/com/example/skinlab/Forum_AddReview_Main.java
+++ app/src/main/java/com/example/skinlab/Forum_AddReview_Main.java
@@ -3,6 +3,7 @@ package com.example.skinlab;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -30,25 +31,29 @@ public class Forum_AddReview_Main extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 showAlerDialog();
+                Intent intent = new Intent(Forum_AddReview_Main.this, Forum_AddReview.class);
+                startActivity(intent);
             }
-            private void showAlerDialog() {
-                ActivityForumDialogSendBinding forumdialogsendBinding = ActivityForumDialogSendBinding.inflate(LayoutInflater.from(Forum_AddReview_Main.this));
-                AlertDialog.Builder builder = new AlertDialog.Builder(Forum_AddReview_Main.this)
-                        .setView(forumdialogsendBinding.getRoot())
-                        .setCancelable(true);
 
-                // Create dialog
-                final AlertDialog dialog = builder.create();
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.getWindow().setLayout(200, 200);
-                dialog.show();
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        dialog.dismiss();
-                    }
-                }, 1000);
-            }
         });
+    }
+
+    private void showAlerDialog() {
+        ActivityForumDialogSendBinding forumdialogsendBinding = ActivityForumDialogSendBinding.inflate(LayoutInflater.from(Forum_AddReview_Main.this));
+        AlertDialog.Builder builder = new AlertDialog.Builder(Forum_AddReview_Main.this)
+                .setView(forumdialogsendBinding.getRoot())
+                .setCancelable(true);
+
+        // Create dialog
+        final AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().setLayout(200, 200);
+        dialog.show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                dialog.dismiss();
+            }
+        }, 1000);
     }
 }
