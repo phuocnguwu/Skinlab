88c7802470a4351b819b722d219633bd9c96e93f
nhudh <nhudh21411@st.uel.edu.vn>
2024-04-13 09:01:05 +0700

    Spinner

---
+++ app/src/main/java/com/example/adapters/PaymentMethodsAdapter.java
@@ -0,0 +1,9 @@
+package com.example.adapters;
+
+public class PaymentMethodsAdapter {
+
+//    Spinner spinner = findViewById(R.id.spinner);
+//    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner_items, android.R.layout.simple_spinner_item);
+//    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//    spinner.setAdapter(adapter);
+}
