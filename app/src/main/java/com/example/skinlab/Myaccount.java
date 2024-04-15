884d926c7532b3e37ac2e5987150797113d0cf1d
nguyenvumaithanh <thanhnvm21411@st.uel.edu.vn>
2024-04-15 19:01:52 +0700

    update avatar

--- app/src/main/java/com/example/skinlab/Myaccount.java
+++ app/src/main/java/com/example/skinlab/Myaccount.java
@@ -1,16 +1,30 @@
 package com.example.skinlab;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.skinlab.databinding.ActivityMyaccountBinding;
 
 public class Myaccount extends AppCompatActivity {
 
     ActivityMyaccountBinding binding;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    boolean OpenCam;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +32,23 @@ public class Myaccount extends AppCompatActivity {
         binding = ActivityMyaccountBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         addEvents();
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        if (OpenCam) {
+                            Bitmap photo = (Bitmap) result.getData().getExtras().get("data");
+                            binding.imvavatar.setImageBitmap(photo);
+                        } else {
+                            Uri selectedPhotoUri = result.getData().getData();
+                            binding.imvavatar.setImageURI(selectedPhotoUri);
+                        }
+
+                    }
+
+                }
+        );
     }
 
     private void addEvents() {
-        binding.btneditavatar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
         binding.moronghoso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,6 +59,8 @@ public class Myaccount extends AppCompatActivity {
         binding.morongdonhang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(Myaccount.this, Donhang_dathang.class);
+                startActivity(intent);
 
             }
         });
@@ -59,6 +83,45 @@ public class Myaccount extends AppCompatActivity {
             public void onClick(View v) {
 
             }
+        });
+        binding.btneditavatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheet();
+
+            }
+
+            private void showBottomSheet() {
+                Dialog dialog = new Dialog(Myaccount.this);
+                dialog.setContentView(R.layout.bottomsheet_dialog);
+
+                LinearLayout bsCam = dialog.findViewById(R.id.bsCam);
+                bsCam.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        OpenCam = true;
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                        activityResultLauncher.launch(intent);
+                        dialog.dismiss();
+                    }
+                });
+
+                LinearLayout bsGal = dialog.findViewById(R.id.bsGallery);
+                bsGal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        activityResultLauncher.launch(intent);
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().setGravity(Gravity.BOTTOM);
+            }
+
         });
     }
 }
--- app/src/main/java/com/example/skinlab/Myaccount_Profile.java
+++ app/src/main/java/com/example/skinlab/Myaccount_Profile.java
@@ -1,15 +1,24 @@
 package com.example.skinlab;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.skinlab.databinding.ActivityDialogSaveBinding;
 import com.example.skinlab.databinding.ActivityMyaccountBinding;
@@ -17,6 +26,8 @@ import com.example.skinlab.databinding.ActivityMyaccountProfileBinding;
 
 public class Myaccount_Profile extends AppCompatActivity {
     ActivityMyaccountProfileBinding binding;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    boolean OpenCam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +35,67 @@ public class Myaccount_Profile extends AppCompatActivity {
         binding = ActivityMyaccountProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         addEvents();
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        if (OpenCam) {
+                            Bitmap photo = (Bitmap) result.getData().getExtras().get("data");
+                            binding.imvavatar.setImageBitmap(photo);
+                        } else {
+                            Uri selectedPhotoUri = result.getData().getData();
+                            binding.imvavatar.setImageURI(selectedPhotoUri);
+                        }
+
+                    }
+
+                }
+        );
     }
 
+
     private void addEvents() {
+        binding.btneditavatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheet();
+
+            }
+
+            private void showBottomSheet() {
+                Dialog dialog = new Dialog(Myaccount_Profile.this);
+                dialog.setContentView(R.layout.bottomsheet_dialog);
+
+                LinearLayout bsCam = dialog.findViewById(R.id.bsCam);
+                bsCam.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        OpenCam = true;
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                        activityResultLauncher.launch(intent);
+                        dialog.dismiss();
+                    }
+                });
+
+                LinearLayout bsGal = dialog.findViewById(R.id.bsGallery);
+                bsGal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        activityResultLauncher.launch(intent);
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().setGravity(Gravity.BOTTOM);
+            }
+
+        });
         binding.btnclickback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Myaccount_Profile.this,Myaccount.class);
+                Intent intent = new Intent(Myaccount_Profile.this, Myaccount.class);
                 startActivity(intent);
             }
         });
@@ -51,16 +116,11 @@ public class Myaccount_Profile extends AppCompatActivity {
                 binding.txtEmail.setText(newemail);
                 showAlerDialog();
             }
-
-
-
             private void showAlerDialog() {
                 ActivityDialogSaveBinding dialogsaveBinding = ActivityDialogSaveBinding.inflate(LayoutInflater.from(Myaccount_Profile.this));
                 AlertDialog.Builder builder = new AlertDialog.Builder(Myaccount_Profile.this)
                         .setView(dialogsaveBinding.getRoot())
                         .setCancelable(true);
-
-                // Create dialog
                 final AlertDialog dialog = builder.create();
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.getWindow().setLayout(200, 200);
@@ -72,10 +132,6 @@ public class Myaccount_Profile extends AppCompatActivity {
                     }
                 }, 1000);
             }
-
-
-
-
         });
     }
 }
