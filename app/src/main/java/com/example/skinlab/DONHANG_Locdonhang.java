507559de1a0f2b50a184a576e3bf09af7bfdceab
phuocng216 <phuocxyzz@example.com>
2024-04-24 22:18:06 +0700

    order 1

--- app/src/main/java/com/example/skinlab/DONHANG_Locdonhang.java
+++ app/src/main/java/com/example/skinlab/DONHANG_Locdonhang.java
@@ -19,7 +19,6 @@ import java.util.List;
 public class DONHANG_Locdonhang extends AppCompatActivity {
 
     ActivityMainContaintFragmentBinding binding;
-    Spinner spncategory;
     CategoryAdapter adapter;
 
     @Override
@@ -27,30 +26,5 @@ public class DONHANG_Locdonhang extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainContaintFragmentBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        spncategory = findViewById(R.id.spn_category);
-        adapter = new CategoryAdapter(this, R.layout.donhang_category_selecteditem, getListCategory());
-        spncategory.setAdapter(adapter);
-        spncategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Toast.makeText(DONHANG_Locdonhang.this, "Vui lòng chọn bộ lọc!", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private List<Category> getListCategory() {
-        List<Category> list = new ArrayList<>();
-        list.add(new Category("Đang xử lý"));
-        list.add(new Category("Đang vận chuyển"));
-        list.add(new Category("Đang giao hàng"));
-        list.add(new Category("Giao hàng thành công"));
-        list.add(new Category("Đã hủy"));
-        return list;
     }
 }
